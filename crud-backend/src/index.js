2d5b7549d8426ab748222ae11c5785476e6741f9
Kalfontein Cruz <cruz.310690@caloocan.sti.edu.ph>
2025-06-11 00:58:33 +0800

    back-end

---
+++ crud-backend/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => (
+    res.send('<h1>Hello Backend!</h1>')
+));
+
+app.listen(port, () => {
+    console.log("Listening on port " + port); 
+});
+
